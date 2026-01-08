bfccd811d5dc8216c2263f87fa8920dd656c8ee0
Ansh Gupta <144530446+Anshgupta2906@users.noreply.github.com>
2026-01-08 13:10:20 +0530

    Add files via upload

---
+++ dsa/arrays/sum_of_array.py
@@ -0,0 +1,13 @@
+# Problem: Sum of elements in an array
+# Approach:
+# - initialize total = 0
+# - loop through array
+# - add each element
+
+arr = [1, 2, 3, 4]
+
+total = 0
+for num in arr:
+    total += num
+
+print(total)
